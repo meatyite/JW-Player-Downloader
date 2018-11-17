4a1c0e8d28f6399ee79b36737e11e45f68eaf069
sl4vkek <43294802+sl4vkek@users.noreply.github.com>
2018-11-17 21:20:47 +0200

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# In this script, it uses command line arguments instead of user input for downloading the video
+
 ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i "$1" -c copy "$2";
